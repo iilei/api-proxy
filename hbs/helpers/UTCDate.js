543c5bc622c631aa4b802bea0c31fad6a7efd49d
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2018-12-24 12:38:39 +0100

    Minor revision on Date handling helper

--- hbs/helpers/UTCDate.js
+++ hbs/helpers/UTCDate.js
@@ -1,27 +1,21 @@
 var UTCDate = function () {};
 
-function dateExample() {
-  var d = new Date();
-  return `${d.getUTCFullYear()}-${d.getUTCMonth() + 1}-${d.getUTCDate()}`
-}
-
 UTCDate.register = function (Handlebars) {
     Handlebars.registerHelper('UTCDate', function(value, block) {
-      const result = (new Date(value)).toUTCString() || "Invalid Date";
+      // toGMTString ~= RFC 1123 - in respect to HTTP Date Header convention
+      const result = (new Date(value)).toGMTString() || "Invalid Date";
 
       if (result === "Invalid Date") {
         throw new Error([
           `Invalid Date provded: "${value}"`,
           `To generate a valid Date, generate a valid date-time:`,
-          `>  node -e "console.log(new Date('${dateExample()}'))" `,
+          `>  node -e "console.log(new Date('YYYY-MM-DD'))" `,
           `Or:`,
-          `    ${new Date(dateExample()).toGMTString()}`,
+          `    ${new Date().toGMTString()}`,
           ].join('\n')
         );
       }
-
-      // toUTCString ~= RFC 1123 - in respect to HTTP Date Header convention
-      return (new Date(value)).toGMTString();
+      return result;
     });
 };
 
